2e75b0ade37d0d60356dc7055c9bb2ee244f224a
BagusSatria123 <satriabagusputra123@gmail.com>
2020-10-28 23:29:37 +0700

    fetch API Aladhan dan menampilkan jadwal sholat

--- app.js
+++ app.js
@@ -1,9 +1,45 @@
+function prayerTimes(latitude, longitude) {
+  fetch(
+    "http://api.aladhan.com/v1/calendar?latitude=" +
+      latitude +
+      "&longitude=" +
+      longitude +
+      "&method=2"
+  )
+    .then((response) => response.json())
+    .then(function (response) {
+      //   console.log(response.data[1].timings);
+      let date = new Date();
+      let today = date.getDate() - 1;
+      //   console.log(today);
+      //   console.log(response.data[today]);
+    });
+}
+function success(position) {
+  prayerTimes(position.coords.latitude, position.coords.longitude);
+}
+
+function error() {
+  alert("Posisi tidak dapat diakses");
+}
+
+function userLocation() {
+  if (!navigator.geolocation) {
+    alert(
+      "Geolocation tidak di dukung didalam browser anda, silahkan gunakan browser lainnya"
+    );
+  } else {
+    navigator.geolocation.getCurrentPosition(success, error);
+  }
+}
 function index() {
   let app = document.getElementById("app");
   let h3 = document.createElement("h3");
   h3.innerHTML = "Prayer Times";
 
   app.appendChild(h3);
+
+  userLocation();
 }
 
 index();
