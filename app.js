58e50c4d85e40d4c7025449cdab421300fa851a0
BagusSatria123 <satriabagusputra123@gmail.com>
2020-10-29 00:13:05 +0700

    bikin HTML jadwal sholat dan ganti nilai parameter method API

--- app.js
+++ app.js
@@ -4,13 +4,30 @@ function prayerTimes(latitude, longitude) {
       latitude +
       "&longitude=" +
       longitude +
-      "&method=2"
+      "&method=4"
   )
     .then((response) => response.json())
     .then(function (response) {
       //   console.log(response.data[1].timings);
       let date = new Date();
-      let today = date.getDate() - 1;
+      let today = date.getDate();
+      let data = response.data[0].timings;
+
+      let app = document.getElementById("app");
+      let table = document.createElement("table");
+      let tableTbody = document.createElement("tbody");
+
+      for (i in data) {
+        let row = tableTbody.insertRow();
+        let name = row.insertCell(0);
+        let time = row.insertCell(1);
+        name.innerHTML = i;
+        time.innerHTML = data[i];
+        tableTbody.appendChild(row);
+      }
+
+      table.appendChild(tableTbody);
+      app.appendChild(table);
       //   console.log(today);
       //   console.log(response.data[today]);
     });
