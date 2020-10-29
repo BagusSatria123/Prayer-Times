cd67a7d9095c95a032f1fcd8717d642ccfc92775
BagusSatria123 <satriabagusputra123@gmail.com>
2020-10-29 10:33:18 +0700

    App css Done semua

--- app.js
+++ app.js
@@ -38,6 +38,8 @@ function success(position) {
 
 function error() {
   alert("Posisi tidak dapat diakses");
+  //default meggunakan latitude jakarta
+  //   prayerTimes("-6.200000", "106.816666");
 }
 
 function userLocation() {
